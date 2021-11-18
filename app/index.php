66bd0d527f07f194bf90a6405d1a5e9c52d1c6e4
Benjamin Daschel <ben@lunarbyte.io>
2021-11-18 12:02:44 -0800

    lesson 1: very basic PHP setup

---
+++ app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Playground</title>
+</head>
+<body>
+
+<h1>
+    Welcome to PHP Playground!
+</h1>
+
+<form method="post">
+    <div>
+        <label for="name">Your Name</label>
+        <input name="name" id="name">
+    </div>
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
